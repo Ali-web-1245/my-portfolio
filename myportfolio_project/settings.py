98cf7db0f90d802a84f2184c6f886ef109ff6155
M.Ali <alirajput8091@gmail.com>
2026-07-29 12:59:06 -0700

    Configure settings for Render

--- myportfolio_project/settings.py
+++ myportfolio_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1a)qq0(+u2y(42t#@%5frv(-^d552z=y+n+w&a6q$^@pp8-w=^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -116,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Media files (Uploaded Images)
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
